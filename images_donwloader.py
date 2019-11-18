fca7f97448f74787b5603a17dd356e2aa76c92af
Gerald <29520085+Gelardinio@users.noreply.github.com>
2019-11-18 11:22:07 -0500

    Major edits

--- images_donwloader.py
+++ images_donwloader.py
@@ -1,28 +1,21 @@
 import tkinter as tk
 import os, sys
+import cv2
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 import numpy as np
 
 os.chdir('C:\ISU')
 
-if False: 
-    master = tk.Tk()
-    master.title("ISU")
-    tk.Label(master, text="First Keyword").grid(row=0)
-    tk.Label(master, text="Second Keyword").grid(row=1)
+KEYWORDS = []
 
-    e1 = tk.Entry(master)
-    e2 = tk.Entry(master)
+global isRun
+isRun = False
 
-    e1.grid(row=0, column=1)
-    e2.grid(row=1, column=1)
-
-    master.mainloop()
-Keyword1_Directory = "Images/Keyword1"
-Keyword2_Directory = "Images/Keyword2"
-Output_Directory  = "ImageOutput"
-Main_Image_Output = "MainOutput"
+Keyword1_Directory = "Main/Images/Keyword1"
+Keyword2_Directory = "Main/Images/Keyword2"
+Output_Directory  = "Main/ImageOutput"
+Main_Image_Output = "Main/MainOutput"
 
 from google_images_download import google_images_download
 
@@ -37,174 +30,224 @@ def create_folders(folder_name):
     except OSError:
         print ('Error: Creating directory. ' +  folder_name)
 
-KEYWORDS = []
-
 create_folders("Images")
 create_folders("ImageOutput")
 create_folders("MainOutput")
 
-buffer_word = input("Enter the first keyword: ")
-KEYWORDS.append(buffer_word)
+count = 0
 
-buffer_word = input("Enter the second keyword: ")
-KEYWORDS.append(buffer_word)
+def _from_rgb(rgb):
+    return "#%02x%02x%02x" % rgb   
+
+def runPrg(): 
+    package=e1.get()
+    KEYWORDS.append(package)
+    package=e2.get()
+    KEYWORDS.append(package)
+    global isRun
+    isRun = True
+
+def close_window(): 
+    master.destroy()
+
+master = tk.Tk()
+master.title("Mosaic Maker")
+
+master.configure(background=_from_rgb((105, 162, 176)))
+
+master.minsize(500, 200)
+
+Label1 = tk.Label(master, text="First Keyword", font=("Courier", 20), background=_from_rgb((105, 162, 176))).grid(row=0, column=0)
+
+Label2 = tk.Label(master, text="Second Keyword", font=("Courier", 20), background=_from_rgb((105, 162, 176))).grid(row=1, column=0)
+
+quitButton = tk.Button(master, text="Quit",font=("Courier", 20), command = close_window)
+quitButton.configure(background= _from_rgb((160, 163, 161))) 
+quitButton.place(x=10, y= 140)
+
+runButton = tk.Button(master, text="Run",font=("Courier", 20), command = runPrg)
+runButton.configure(background=_from_rgb((101, 145, 87)))
+runButton.place(x=320, y=140)
+runButton.config(width = 10, height = 1)
+
+e1 = tk.Entry(master,width=20, font=("Courier", 15), background=_from_rgb((48, 189, 240)))
+e1.place(x = 240, y = 9)
+e2 = tk.Entry(master,width=20, font=("Courier", 15), background=_from_rgb((48, 189, 240)))
+e2.place(x = 240, y = 42)
 
 print(KEYWORDS)
 
-arguments = {
-    "keywords":KEYWORDS[0],
-    "limit":1,
-    "output_directory":'Images',
-    "image_directory":'Keyword1',
-    "type":'photo',
-    "format":'jpg',
-    "print_urls": False,
-}
+if isRun == True:
 
-download_images(arguments)
+    print(KEYWORDS)
 
-arguments = {
-    "keywords":KEYWORDS[1],
-    "limit":1,
-    "output_directory":'Images',
-    "image_directory":'Keyword2',
-    "print_urls": False,
-    "type":"photo",
-    "format":'jpg',
-}
+    arguments = {
+        "keywords":KEYWORDS[0],
+        "limit":1,
+        "output_directory":'Images',
+        "image_directory":'Keyword1',
+        "type":'photo',
+        "format":'jpg',
+        "print_urls": False,
+    }
 
-download_images(arguments)
-
-for file in os.listdir(Keyword1_Directory):
-    img = Image.open(os.path.join(Keyword1_Directory, file))
-    img = img.resize((50,50))
-    os.path.join(Output_Directory, file)
-    img.save(os.path.join(Output_Directory, file))  
-
-
-count = 0
-arr_count = 0
-averageR = 0
-averageG = 0
-averageB = 0
-
-arr_R = []
-arr_G = []
-arr_B = []
-
-for file in os.listdir(Output_Directory):
-    
-    img = Image.open(os.path.join(Output_Directory, file))
-    img = img.convert('RGB')
-    for y in range(0, 50):
-        for x in range(0, 50):
-    
-            RGB = img.getpixel((x,y))
-            r, g, b = RGB
+    while count < 1: 
+        download_images(arguments)
+        for file in os.listdir(Keyword1_Directory):
+            img = Image.open(os.path.join(Keyword1_Directory, file))
             count = count + 1
 
-            averageR += r
-            averageG += g
-            averageB += b
 
-    averageR = averageR / count
-    averageG = averageG / count
-    averageB = averageB / count
+    arguments = {
+        "keywords":KEYWORDS[1],
+        "limit":1,
+        "output_directory":'Images',
+        "image_directory":'Keyword2',
+        "print_urls": False,
+        "type":"photo",
+        "format":'jpg',
+    }
 
-    arr_R.append(averageR)
-    arr_G.append(averageG)
-    arr_B.append(averageB)
+    count = 0
 
-    print(str(int(arr_R[arr_count])) + ',' + str(int(arr_G[arr_count])) + ',' + str(int(arr_B[arr_count])))
+    while count < 1: 
+        download_images(arguments)
+        for file in os.listdir(Keyword2_Directory):
+            img = Image.open(os.path.join(Keyword2_Directory, file))
+            count = count + 1
 
-    arr_count = arr_count + 1
+
+    for file in os.listdir(Keyword1_Directory):
+        img = Image.open(os.path.join(Keyword1_Directory, file))
+        img = img.resize((50,50))
+        os.path.join(Output_Directory, file)
+        img.save(os.path.join(Output_Directory, file))  
+
+
+    count = 0
+    arr_count = 0
+    averageR = 0
+    averageG = 0
+    averageB = 0
+
+    arr_R = []
+    arr_G = []
+    arr_B = []
+
+    for file in os.listdir(Output_Directory):
+        
+        img = Image.open(os.path.join(Output_Directory, file))
+        img = img.convert('RGB')
+        for y in range(0, 50):
+            for x in range(0, 50):
+        
+                RGB = img.getpixel((x,y))
+                r, g, b = RGB
+                count = count + 1
+
+                averageR += r
+                averageG += g
+                averageB += b
+
+        averageR = averageR / count
+        averageG = averageG / count
+        averageB = averageB / count
+
+        arr_R.append(averageR)
+        arr_G.append(averageG)
+        arr_B.append(averageB)
+
+        arr_count = arr_count + 1
+
+        count = 0
+        averageR = 0
+        averageG = 0
+        averageB = 0
 
     count = 0
     averageR = 0
     averageG = 0
     averageB = 0
 
-count = 0
-averageR = 0
-averageG = 0
-averageB = 0
+    f = open("values.txt", "w")
 
-f = open("values.txt", "w")
+    for file in os.listdir(Keyword2_Directory):
+        img = Image.open(os.path.join(Keyword2_Directory, file))
+        img = img.convert('RGB')
+        width, height = img.size
+        width = 3 * round(width/3) 
+        height = 3 * round(height/3) 
+        print(width, height)
+        img = img.resize((width, height))
+        rgbr = [[0 for x in range((int(width/3)))] for y in range(int((height/3)))]
+        rgbb = [[0 for x in range((int(width/3)))] for y in range(int((height/3)))]
+        rgbg = [[0 for x in range((int(width/3)))] for y in range(int((height/3)))]
+        for y in range(0, height):
+            for x in range(0, width):
 
-for file in os.listdir(Keyword2_Directory):
-    img = Image.open(os.path.join(Keyword2_Directory, file))
-    img = img.convert('RGB')
-    width, height = img.size
-    width = 3 * round(width/3) 
-    height = 3 * round(height/3) 
-    print(width, height)
-    img = img.resize((width, height))
-    rgbr = [[0 for x in range((int(width/3)))] for y in range(int((height/3)))]
-    rgbb = [[0 for x in range((int(width/3)))] for y in range(int((height/3)))]
-    rgbg = [[0 for x in range((int(width/3)))] for y in range(int((height/3)))]
-    for y in range(0, height):
-        for x in range(0, width):
+                RGB = img.getpixel((x,y))
+                r, g, b = RGB
+                count = count + 1
 
-            RGB = img.getpixel((x,y))
-            r, g, b = RGB
-            count = count + 1
+                averageR += r
+                averageG += g
+                averageB += b
 
-            averageR += r
-            averageG += g
-            averageB += b
+                if x % 3 == 0:
+                    rgbr[int(y / 3)][int(x / 3)] = (averageR / 3)
+                    rgbg[int(y / 3)][int(x / 3)] = (averageG / 3)
+                    rgbb[int(y / 3)][int(x / 3)] = (averageB / 3)
+                    averageR = 0
+                    averageG = 0
+                    averageB = 0
 
-            if x % 3 == 0:
-                rgbr[int(y / 3)][int(x / 3)] = (averageR / 3)
-                rgbg[int(y / 3)][int(x / 3)] = (averageG / 3)
-                rgbb[int(y / 3)][int(x / 3)] = (averageB / 3)
-                averageR = 0
-                averageG = 0
-                averageB = 0
+        f.write(str(rgbr[0][0]) + " " + str(rgbg[0][0]) + " " + str(rgbb[0][0]))    
 
-    f.write(str(rgbr[0][0]) + " " + str(rgbg[0][0]) + " " + str(rgbb[0][0]))    
+    f.close()
+    images = []
 
-f.close()
-images = []
+    for file in os.listdir(Output_Directory):
+        img2 = Image.open(os.path.join(Output_Directory, file))
+        images.append(img2)
 
-for file in os.listdir(Output_Directory):
-    img2 = Image.open(os.path.join(Output_Directory, file))
-    images.append(img2)
-
-for file in os.listdir(Keyword2_Directory):
-    img = Image.open(os.path.join(Keyword2_Directory, file))
-    img = img.convert('RGB')
-    width, height = img.size
-    width = 3 * round(width/3) 
-    height = 3 * round(height/3) 
-    total_diff = []
-    x_offset = 0
-    y_offset = 0
-
-    img2 = Image.new('RGB', ((width * 16), (height * 16)))
-
-    for y in range(0, (int(height / 3))):  
+    for file in os.listdir(Keyword2_Directory):
+        img = Image.open(os.path.join(Keyword2_Directory, file))
+        img = img.convert('RGB')
+        width, height = img.size
+        width = 3 * round(width/3) 
+        height = 3 * round(height/3) 
+        total_diff = []
         x_offset = 0
-        for x in range(0, (int(width / 3))): 
-            index_total = 0
-            difference_total = (abs((rgbr[y][x] - arr_R[0])) + abs((rgbg[y][x] - arr_G[0])) + abs((rgbb[y][x] - arr_B[0])))
-            for i in range(0, len(arr_R)):
-                if (abs((rgbr[y][x] - arr_R[i])) + abs((rgbg[y][x] - arr_G[i])) + abs((rgbb[y][x] - arr_B[i]))) < difference_total: 
-                    difference_total = (abs((rgbr[y][x] - arr_R[i])) + abs((rgbg[y][x] - arr_G[i])) + abs((rgbb[y][x] - arr_B[i])))
-                    index_total = i
+        y_offset = 0
 
-            img2.paste(images[index_total], (x_offset, y_offset))
-            x_offset = x_offset + 50
-                
-        print(str(int(difference_total)))
-        y_offset = y_offset + 50 
-    img2.save('firsttest.jpg')
-        
+        img2 = Image.new('RGB', ((width * 16), (height * 16)))
+
+        for y in range(0, (int(height / 3))):  
+            x_offset = 0
+            for x in range(0, (int(width / 3))): 
+                index_total = 0
+                difference_total = (abs((rgbr[y][x] - arr_R[0])) + abs((rgbg[y][x] - arr_G[0])) + abs((rgbb[y][x] - arr_B[0])))
+                for i in range(0, len(arr_R)):
+                    if (abs((rgbr[y][x] - arr_R[i])) + abs((rgbg[y][x] - arr_G[i])) + abs((rgbb[y][x] - arr_B[i]))) < difference_total: 
+                        difference_total = (abs((rgbr[y][x] - arr_R[i])) + abs((rgbg[y][x] - arr_G[i])) + abs((rgbb[y][x] - arr_B[i])))
+                        index_total = i
+
+                img2.paste(images[index_total], (x_offset, y_offset))
+                x_offset = x_offset + 50
+                    
+            print(str(int(difference_total)))
+            y_offset = y_offset + 50 
+        img2.save('firsttest.jpg')
+            
 
 
-#os.remove(Keyword1_Directory)
-#os.remove(Keyword2_Directory)
-#os.remove(Output_Directory)
-#os.remove(Main_Image_Output)
+    #os.remove(Keyword1_Directory)
+    #os.remove(Keyword2_Directory)
+    #os.remove(Output_Directory)
+    #os.remove(Main_Image_Output)
 
 
+
+master.mainloop()
 print("done")
+ 
